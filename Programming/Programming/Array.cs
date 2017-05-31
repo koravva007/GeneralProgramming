ece654c2ace397809c4e46d2b27b54294e669280
Kondala Rao Ravva <koravva@microsoft.com>
2017-05-31 20:30:18 +0530

    checking in changes

---
+++ Programming/Programming/Array.cs
@@ -0,0 +1,48 @@
+﻿namespace Programming
+{
+    public class Array
+    {
+        public static int[] MergeSortedArrays(int[] arr1, int[] arr2)
+        {
+            int arrayLength = arr1.Length + arr2.Length;
+            int[] result = new int[arrayLength];
+
+            int i=0, j = 0, k = 0;
+            
+            while((i< arr1.Length) && (j < arr2.Length))
+            {
+                if(arr1[i]<arr2[j])
+                {
+                    result[k] = arr1[i];
+                    i++;
+                    k++;
+                }
+                else
+                {
+                    result[k] = arr2[j];
+                    j++;
+                    k++;
+                }
+            }
+            if(i< arr1.Length)
+            {
+                while(i< arr1.Length)
+                {
+                    result[k] = arr1[i];
+                    i++;
+                    k++;
+                }
+            }
+            else
+            {
+                while(j < arr2.Length)
+                {
+                    result[k] = arr2[j];
+                    j++;
+                    k++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Programming/Programming/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 5, 8, 9 };
+            int[] arr2 = { 4, 7, 8 };
+            int[] arr3 = Array.MergeSortedArrays(arr1, arr2);
+        }
+    }
+}
